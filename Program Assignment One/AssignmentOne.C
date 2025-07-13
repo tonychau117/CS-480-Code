4b7293b320346ddf5e0d7e897b6db8a44852e508
Tony Chau <tonychau@MacBookAir.lan>
2025-07-13 16:56:03 -0700

    read me + flag create

--- Program Assignment One/AssignmentOne.C
+++ Program Assignment One/AssignmentOne.C
@@ -3,20 +3,20 @@
 
 #include <semaphore.h> // import for semaphore flag
 
-
-int  main(){
+int main()
+{
     FILE *fptr;
 
     fptr = fopen("QUOTE.txt", "w"); // creates the QUOTE.txt file, w used to write in it -> if doesnt exist itll amke one
-    pid_t processID = getpid(); // gets the process ID
+    pid_t processID = getpid();     // gets the process ID
 
     fprintf(fptr, "The ProcessID is" + processID);
-    fprint(fptr, "\r\n"); // carriage return and newline
-    fclose(fptr); // closes the file that we are writing in
+    fprintf(fptr, "\r\n"); // carriage return and newline, carriage return with \r
+    fclose(fptr);          // closes the file that we are writing in
 
-    // carriage return done using \r
-
-
+    // creating the semaphore FLAG
+    sem_t sem_flag;
+    sem_init(&sem_flag, 0, 0);
 
     printf("Hello World");
 }
---
+++ README.txt
@@ -0,0 +1,21 @@
+File Manifest:
+- AssignmentOne.C
+
+Compile Instructions:
+- Have a working environment of C installed
+
+Operating Instructions:
+-
+
+List/description of novel/significant design decisions:
+- 
+
+List/description of any extra features/algorithms/functionality you included which were not
+required:
+- 
+
+List/description of all known deficiencies or bugs:
+- 
+
+Lessons Learned:
+- 
