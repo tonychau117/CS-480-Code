bd7a7626e777ba040a759aa3bff21e11b57b3aa7
Generic Account 1455 <cssc1455@edoras.sdsu.edu>
2025-07-15 21:19:37 -0700

    Added code to main and a small fix, implemented thread_function

--- Program Assignment One/AssignmentOne.C
+++ Program Assignment One/AssignmentOne.C
@@ -3,66 +3,91 @@
 
 #include <semaphore.h> // import for semaphore flag
 #include <pthread.h> // import for creating the threads
+#include <stdlib.h> // import for exit() and dynamic memory
 
 #define NUMBER_OF_THREADS 7 // number of threads
-#define EVEN_QUOTE "“Controlling complexity is the essence of computer programming.” --Brian Kernighan"
-#define ODD_QUOTE "“Computer science is no more about computers than astronomy is about telescopes.” --Edsger Dijkstra"
+#define EVEN_QUOTE "\"Controlling complexity is the essence of computer programming.\" --Brian Kernighan"
+#define ODD_QUOTE "\"Computer science is no more about computers than astronomy is about telescopes.\" --Edsger Dijkstra"
 
-sem_t sem_flag;
+sem_t sem_flag; // global semaphore
 
 int main()
 {
-    FILE *fptr = fopen("QUOTE.txt", "w"); // creates the QUOTE.txt file, w used to write in it -> if doesnt exist itll amke one   
-
-    fprintf(fptr, "The ProcessID is: ", getpid()); // gets the process ID
+    FILE *fptr = fopen("QUOTE.txt", "w"); // creates the QUOTE.txt file, w used to write in it -> if doesnt exist itll amke one
+    if (fptr == NULL) { // checks if file was created
+    	perror("QUOTE.txt failed");
+    	exit(EXIT_FAILURE);
+    }
+    fprintf(fptr, "The ProcessID is: %d ", getpid()); // gets the process ID
     fprintf(fptr, "\r\n"); // carriage return and newline, carriage return with \r
     fclose(fptr);          // closes the file that we are writing in
 
     // creating the semaphore FLAG - maybe give it global scope
-    sem_t sem_flag;
-    sem_init(&sem_flag, 0, 0);
+    if (sem_init(&sem_flag, 0, 1) != 0) { // error check for semaphore
+    	perror("Semaphore creation failed");
+    	exit(EXIT_FAILURE);
+    }
 
     // creating threads
     pthread_t threads[NUMBER_OF_THREADS]; // declares seven threads to use
-    for(int i = 0; i < NUMBER_OF_THREADS; i++)
-    {
-        pthread_create(threads[i], NULL, thread_function, (void*)i); // initializes them, passes in the thread # for the argument of the fucntion, maybe change last argument
+    for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+    	int *thread_num = malloc(sizeof(int)); // memory allocation
+    	*thread_num = i; // assigns thread number
+    	if (pthread_create(&threads[i], NULL, thread_function, (void *)thread_num) !=0) { //error check
+    		perror("Thread creation failed");
+    		exit(EXIT_FAILURE);
+    	}
     }
 
-
-
-
-
-
-
+    // joining thread
+    for(int i = 0; i < NUMBER_OF_THREADS; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
 
     // destroying the semaphore
     sem_destroy(&sem_flag);
+    printf("Threads successfully completed. Program exiting.\n");
+    return 0;
 }
 
 // quote even - “Controlling complexity is the essence of computer programming. --Brian Kernighan
 // quote odd - “Computer science is no more about computers than astronomy is about telescopes.” --Edsger Dijkstra
 
 // thread function for what the thread is going to do
-void *thread_function(void* tid)
+void *thread_function(void *arg)
 {
     // even threads - once every 2 seconds
     // odd threads - once every 3 seconnds
-    if(<thread_id> % 2 == 0) // do something about finding the argument for getting the thread id
-    {
+    int thread_id = *(int *)arg; // get thread ID from arg
+    free(arg); // free allocated memory
+
+    for(int i = 0; i < 8; i++) { // run 8 times
+    	if (thread_id % 2 == 0) {
+    		 sleep(2); // even threads wait 2 seconds
+    	}
+    	else {
+    		sleep(3); // odd threads wait 3 seconds
+    	}
+
+    	sem_wait(&sem_flag); // lock semaphore
+
+    	FILE *fptr = fopen("QUOTE.txt", "a"); // opens the QUOTE file
+    	if (fptr == NULL) {
+    		sem_post(&sem_flag);
+    		pthread_exit(NULL);
+    	}
+
+    	fprintf(fptr, "Thread ID %d - ", thread_id); // prints thread ID
+    	fprintf(fptr, "%s\r\n", (thread_id % 2 == 0) ? EVEN_QUOTE : ODD_QUOTE); // chooses quote
+    	fclose(fptr); // close file
+
+    	printf("Thread %d is running\n", thread_id);
+
+    	sem_post(&sem_flag); // release the semaphore flag
 
     }
-    else
-    {
 
-    }
-
-    FILE *fptr = fopen("QUOTE.txt", "w"); // opens the QUOTE file
-    
-    fprintf(fptr, "\r\nThread ID: ", pthread_self(), <insert the quote here depending on thread even/odd); // writing hte thread id, quote, carr return + newline
-    printf("\nThread <get the number of the thread here> is running"); // writing to the console
-    fclose(fptr); // closes the file
-
-    // release the semaphore flah
-    
+    pthread_exit(NULL);
 }
+
