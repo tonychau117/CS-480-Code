4eb51e8bf4742987df2c63b4546708a828d2c4f2
tony <tonychau117@gmail.com>
2025-07-21 20:00:55 -0700

    starting to setup assignment2

---
+++ Program Assignment Two/AssignmentTwo.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    printf("cssc0000%"); // prints the cssc when XSH starts running
+
+    // 2
+    // prompt for file name
+    // if executable -> create a new process and run this program in the new process
+    // if not -> repeat asking the question until there is a valid response
+
+    // need to determine if the filename is a fully qualified path name or if t he file needs to be searched for
+    // use correct variant of "exec" to do so
+
+    // 3
+    // XSH will provide a builtin function "pipe" to allow users to create two processes for them to use for communication ebtween the two processes
+
+    // 4
+    // the second builtin function is exit -> type "exit" adn hit enter -> termination
+}
